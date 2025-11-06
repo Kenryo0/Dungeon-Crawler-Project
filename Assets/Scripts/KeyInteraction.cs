92eeac63c1222fe1e53a34e253294d1465d61de1
Kenryo <ledeunff.adrien@gmail.com>
2025-11-06 19:02:29 +0100

    Add Door interaction + Key interaction + Valve interaction with Grid

---
+++ Assets/Scripts/KeyInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInteraction : MonoBehaviour
+{
+    PlayerInventory playerInventory;
+
+    // NearView()
+    float distance;
+
+    private void Start()
+    {
+        playerInventory = FindFirstObjectByType<PlayerInventory>(); // key will get up and it will saved in "inventary"
+    }
+
+    void Update()
+    {
+        if ( NearView() && Input.GetKeyDown(KeyCode.E) )
+        {
+            print("Je suis assez proche et je ramasse la clé");
+            playerInventory.hasKey = true;
+            Destroy(gameObject);
+        }
+    }
+
+    bool NearView() // it is true if you near interactive object
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        if (distance < 2f) return true;
+        else return false;
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [Tooltip("Child empty object for plug following")]
+    public Transform GoalPosition;
+
+    public bool hasKey = false;
+}
